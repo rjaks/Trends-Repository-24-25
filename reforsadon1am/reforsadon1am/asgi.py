1b2635b196c91acdb862ea4c9026f61a815cbe8f
Adrian <107988407+rjaks@users.noreply.github.com>
2025-05-08 00:34:43 +0800

    Add files via upload

---
+++ reforsadon1am/reforsadon1am/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for reforsadon1am project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reforsadon1am.settings')
+
+application = get_asgi_application()
